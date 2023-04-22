196e06c7ce7287cfb73002e23cb889b6a0d51f2d
Felix Villamizar <felixvilla51@gmail.com>
2023-04-22 14:21:48 -0500

    Property Binding y event Binding

--- src/app/Component/persona/persona.component.ts
+++ src/app/Component/persona/persona.component.ts
@@ -3,15 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-persona',
   templateUrl: './persona.component.html',
-  styleUrls: ['./persona.component.css']
+  styleUrls: ['./persona.component.css'],
 })
 export class PersonaComponent implements OnInit {
+  //declareacion de variables
+  nombres: string = 'Felix Joaquin';
+  apellido: string = 'Villamizar Ibanez';
+  edad: number = 40;
+  //private   edad:     number =  25;
 
   constructor() {}
 
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    
-  }
-
+  /* // metodo para Obtener la edad
+     public GetObtenerEdad():number{
+      return this.edad
+     } */
 }
--- src/app/Component/personas/personas.component.ts
+++ src/app/Component/personas/personas.component.ts
@@ -3,15 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
-  styleUrls: ['./personas.component.css']
+  styleUrls: ['./personas.component.css'],
 })
 export class PersonasComponent implements OnInit {
+  // declaracion de variables property binding
+  estado:  boolean =    false;
+  mensaje: string  =    ' No se ha agregado  Ninguna Persona ';
+  titulo:  string  =    ' ';
 
-constructor() {}
+  constructor() {}
 
-ngOnInit(): void {
-  
+  ngOnInit(): void {}
+
+  public agregarPersona() {
+    this.mensaje = 'Persona Agregada'
+  }
+
+
+  public modificarTitulo(event:Event){
+   // alert(event.target + "ES5TE ES MI OBJETO")
+
+   this.titulo =(<HTMLInputElement>event.target).value
+
+
 }
-
 
 }
